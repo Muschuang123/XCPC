7c5ab9a69c8a3c936149da85b6b7c127997bf1c8
Muschuang123 <Muschuang123@163.com>
2025-05-24 20:29:31 +0800

    2025-5-24

--- _Templates/Gauss.cpp
+++ _Templates/Gauss.cpp
@@ -1,3 +1,5 @@
+// Normal Gauss
+
 const ld eps = 1e-8L;
 
 // gauss(a, n).empty() : no solution or infinite solutions
@@ -32,6 +34,62 @@ vector<ld> gauss(vector<vector<ld>> a, int n) {
     vector<ld> ans(n + 1);
     for (int i = 1; i <= n; i++) {
         ans[i] = a[i][n + 1];
+    }
+    return ans;
+}
+
+
+// -------------------------------
+// Gauss-Jordan
+
+const ld eps = 1e-8L;
+
+// vector<vector<ld>> a(n + 1, vector<ld(n + 2));
+// gauss[0] < 0 : no solution
+// gauss[0] > 0 : infinite solutions
+vector<ld> gauss_jordan(vector<vector<ld>> a, int n) {
+    vector<ld> ans(n + 1);
+    // cur 是当前处理的列
+    int cur = 1;
+    // i 是 主元 列 序号
+    for (int i = 1; i <= n; i++) {
+        int fz = cur;
+        for (int k = cur + 1; k <= n; k++) {
+            if (fabsl(a[k][i]) > fabsl(a[fz][i])) {
+                fz = k;
+            }
+        }
+        if (fz != cur) {
+            swap(a[fz], a[cur]);
+        }
+        // 如果没有 非 0 主元，那么保持 cur (当前处理的行)，处理下一列
+        if (fabsl(a[cur][i]) < eps) {
+            continue;
+        }
+        for (int k = 1; k <= n; k++) {
+            if (k == cur) {
+                continue;
+            }
+            ld t = a[k][i] / a[cur][i];
+            for (int j = i; j <= n + 1; j++) {
+                a[k][j] -= t * a[cur][j];
+            }
+        }
+        cur++;
+    }
+    if (cur <= n) {
+        while (cur <= n) {
+            if (fabsl(a[cur][n + 1]) > eps) {
+                ans[0] = -1;
+                return ans;
+            }
+            cur++;
+        }
+        ans[0] = 1;
+        return ans;
+    }
+    for (int i = 1; i <= n; i++) {
+        ans[i] = a[i][n + 1] / a[i][i];
     }
     return ans;
 }
--- _Templates/SparseTable.cpp
+++ _Templates/SparseTable.cpp
@@ -20,3 +20,39 @@ struct SparseTable {
         return max(mx[l][len], mx[r - (1 << len) + 1][len]);
     }
 } ST;
+
+// ---------------------------------------
+// dynamic size version:
+
+// 1-index
+struct SparseTable {
+    vector<vector<int>> mx;
+    vector<int> a, lg;
+    int n;
+
+    // void assign(int _n) {
+    SparseTable(int _n) {
+        n = _n;
+        a.assign(n + 1, 0);
+        mx.assign(n + 1, vector<int>(__lg(n) + 2, 0));
+        lg.assign(n + 1, 0);
+    }
+
+    void build() {
+        lg[0] = -1;
+        for (int i = 1; i <= n; i++) {
+            mx[i][0] = a[i];
+            lg[i] = lg[i / 2] + 1;
+        }
+        for (int i = 1; i <= lg[n]; i++) {
+            for (int j = 1; j + (1 << i) - 1 <= n; j++) {
+                mx[j][i] = max(mx[j][i - 1], mx[j + (1 << (i - 1))][i - 1]);
+            }
+        }
+    }
+
+    int query(int l, int r) {
+        int len = lg[r - l + 1];
+        return max(mx[l][len], mx[r - (1 << len) + 1][len]);
+    }
+};
